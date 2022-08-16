6e5e498296147c6c938a9b29f30913cdd810e7d5
akaaua <akaaua@gmail.com>
2022-08-15 21:20:47 -0400

    Now searching for cities

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+  "method": "GET",
+  "headers": {
+    "X-RapidAPI-Key": "4d2aaf26c0mshee0f6a26a8b1bd8p19cc0ejsn16cd35829549",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+  }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
