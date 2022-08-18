440641a2af2f58efdafa98d1dfa45d74787c233b
akaaua <akaaua@gmail.com>
2022-08-18 16:04:05 -0400

    Adding other elements to the screen and formmating them

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -11,6 +11,31 @@ const CurrentWeather = () => {
         </div>
         <img alt='weather' className='weather-icon' src='icons/01d.png'></img>
       </div>
+      <div className='bottom'>
+      <p className='temperature'>18ºC</p>
+      <div className='details'>
+        <div className='parameter-row'>
+          <span className='parameter-label'>Details</span>
+        </div>
+        <div className='parameter-row'>
+          <span className='parameter-label'>Feels Like</span>
+          <span className='parameter-value'>22ºC</span>
+        </div>
+        <div className='parameter-row'>
+          <span className='parameter-label'>Wind</span>
+          <span className='parameter-value'>2 m/s</span>
+        </div>
+        <div className='parameter-row'>
+          <span className='parameter-label'>Humidity</span>
+          <span className='parameter-value'>15%</span>
+        </div>
+        <div className='parameter-row'>
+          <span className='parameter-label'>Pressure</span>
+          <span className='parameter-value'>15 hPa</span>
+        </div>
+      </div>
+
+      </div>
     </div>
   );
 }
